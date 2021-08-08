128c878fbfc5b267fa012d0d9a7e7df41dd24e1a
marin-8 <marin.tecnico.8@gmail.com>
2021-08-08 22:11:15 +0200

    Avances varios

--- App/App/AssemblyInfo.cs
+++ App/App/AssemblyInfo.cs
@@ -3,7 +3,3 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-
-[assembly: ExportFont("FontIcons_Brands_Regular.otf", Alias = "FontIcons_Brands_Regular")]
-[assembly: ExportFont("FontIcons_Free_Regular.otf", Alias = "FontIcons_Free_Regular")]
-[assembly: ExportFont("FontIcons_Free_Solid.otf", Alias = "FontIcons_Free_Solid")]
