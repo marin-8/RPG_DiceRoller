79f36e56851d4b1906517bb505da883aa5c0711e
marin-8 <marin.tecnico.8@gmail.com>
2021-08-14 18:06:59 +0200

    Avances varios

---
+++ App/App/Screens/Basic/Screen_BasicRolls.xaml.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using App.Enums;
+using App.GUIDs;
+using App.Logic;
+using App.Models;
+
+using Xamarin.Forms;
+
+namespace App.Screens
+{
+	public partial class Screen_BasicRolls : ContentPage
+	{
+		public Screen_BasicRolls()
+		{
+			InitializeComponent();
+			BindingContext = this;
+		}
+	}
+}
